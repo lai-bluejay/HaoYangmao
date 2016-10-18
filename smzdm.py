553236454b9b665b4ffac044205801ed4c1d2a19
lai-bluejay <lai.bluejay@gmail.com>
2016-10-19 01:54:21 +0800

    1. 增加app端签到 2. 增加app端抽奖 3. 开发完成/    公众号端签到无法获取信息

--- smzdm.py
+++ smzdm.py
@@ -18,14 +18,16 @@ logging.basicConfig(level=logging.DEBUG,
                 filename='log/stdout.log',
                 filemode='a')
 console = logging.StreamHandler()
-class Smzdm(object):
 
+class Smzdm(object):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.addHandler(console)
         self.cookies = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
         self.account = dict()
+
+        self.app_url = "https://api.smzdm.com/v1/user/checkin"
         self.headers = {
             'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
             'Referer' : 'http://www.smzdm.com/',
@@ -44,6 +46,13 @@ class Smzdm(object):
             "Accept-Encoding" : " gzip, deflate",
             "Accept-Language" : " zh-CN,zh;q=0.8",
             "Cookie" : " __jsluid=113fe510ed4b4c950c5e46b3fe321626" }
+        self.app_header = {
+            "User-agent" : " smzdm_android_V7.3 rv:345 (MX4 Pro;Android5.1.1;zh)smzdmapp",
+            # "Cookie" : " smzdm_device=android;smzdm_user_source=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG;smzdm_version=7.3;",
+            "Cookie" : " pid=866002025010397;partner_id=0;partner_id=0;device_id=c34f3719f4c71385cfe091758b126a8f;imei=3427b9e3efe644cacf6ff9c0117711e0;partner_name=wandoujia;mac=38:bc:1a:e2:a2:90;smzdm_id=7860525979;login=1;device_push=1;network=wifi;device_smzdm_version=7.3;device_smzdm_version_code=345;device_s=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG;device_type=MeizuMX4 Pro;device_system_version=5.1.1;device_smzdm=android;",
+            "Content-Type" : " application/x-www-form-urlencoded; charset=UTF-8",
+            "Host" : " api.smzdm.com",
+        }
 
     # 登录
     def login(self, account):
@@ -77,6 +86,46 @@ class Smzdm(object):
         url = "http://zhiyou.smzdm.com/user/logout"
         req = urllib2.Request(url, headers = self.headers)
         self.opener.open(req)
+
+    def app_checkin(self):
+        user_name = self.account["username"]
+        post_data = "sk=CUbJSyy13E4CYTGNoJ7tGdgGsw7l5fVfme2KIr%2BeSMU%3D&token=5782ff1812d0e756294&captcha=&f=android&s=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG&weixin=1&v=345&"
+        url = self.app_url
+        # req = requests.post(url, data=post_data, headers=self.app_header)
+        # res = req.read()
+        req = urllib2.Request(url, headers=self.app_header ,data=post_data)
+        res = self.opener.open(req)
+        content = res.read()
+        content_dict = json.loads(content)
+        error_code = content_dict.get("error_code")
+        error_msg = content_dict.get("error_msg")
+        self.logger.error(user_name + error_msg)
+        # 签到之后可以抽奖
+        self.app_lottery()
+
+    def app_lottery(self):
+        user_name = self.account["username"]
+        app_lottery_headers = {
+            "Host": "h5.smzdm.com",
+            "Connection": "keep-alive",
+            "Content-Length": "0",
+            "Accept": "application/json",
+            "Origin" : " https://h5.smzdm.com",
+            "X-Requested-With" : " XMLHttpRequest",
+            "User-Agent" : " Mozilla/5.0 (Linux; Android 5.1.1; MX4 Pro Build/LMY48W) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/40.2214.121 Mobile Safari/537.36 {smzdm_android_V7.3 rv:345 (MX4 Pro;Android5.1.1;zh)smzdmapp}",
+            "Referer" : " https://h5.smzdm.com/user/lottery/checkin?d=SQfD%2FfF%2FdYUUdxOjXVt6IaTReRM2p4dPiYK0KHEyuhC9HwFLwi715A%3D%3D&t=846d09defd4643747eef87eabd2f1f22&f=android&s=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG&add_point=20&displaymode=0&v=345",
+            "Accept-Language": "zh-CN,en-US;q=0.8",
+            "Cookie" : " device_id=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG; partner_id=0; partner_name=wandoujia; smzdm_id=7860525979; device_id=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG; partner_id=0; partner_name=wandoujia; smzdm_id=7860525979; smzdm_user_source=784742AE5BDB6C72DE4163C36CEF4424; PHPSESSID=3jvbg71629noknu1gc84gr0hs3; __jsluid=cfd73a528efc46019ce359b36118b4bb; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1475941209; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1476378465; _ga=GA1.2.1725864596.1469037084; smzdm_wordpress_360d4e510beef4fe51293184b8908074=laihongchang%7C1480697269%7C12ec24e92b0e1c0b77f9aa1af5a6be04; smzdm_wordpress_logged_in_360d4e510beef4fe51293184b8908074=laihongchang%7C1480697269%7C5326e245e45e3d882bedc63acf5aeced; user-role-smzdm=subscriber; user=laihongchang%7C7860525979; from=android; v=345; d=SQfD%2FfF%2FdYUUdxOjXVt6IaTReRM2p4dPiYK0KHEyuhC9HwFLwi715A%3D%3D; t=846d09defd4643747eef87eabd2f1f22; s=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG; sess=; device_id=nKrwch6wzKbD0yhjQMQZXpcILsr5rGwG"
+        }
+        post_data = None
+        lot_url = "https://h5.smzdm.com/user/lottery/ajax_draw"
+        req = urllib2.Request(lot_url, headers=app_lottery_headers, data=post_data)
+        res = self.opener.open(req)
+        content = res.read()
+        content_dict = json.loads(content)
+        error_code = content_dict.get("error_code")
+        error_msg = content_dict.get("error_msg")
+        self.logger.error(user_name + error_msg)
 
     # 签到
     def checkin(self):
@@ -131,6 +180,8 @@ class Smzdm(object):
             account['password'] = parser.get(user, 'password')
             self.login(account)
             self.checkin()
+            self.app_checkin()
+            # self.app_lottery()
             self.is_checkin()
             self.logout()
 if __name__ == '__main__':
